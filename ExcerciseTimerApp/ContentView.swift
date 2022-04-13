dc242301961613972422ba47c3371feccf6253d3
James D Vieyra <james@vieyra.co.za>
2022-04-13 18:22:26 +0200

    Initial Commit

---
+++ ExcerciseTimerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExcerciseTimerApp
+//
+//  Created by IACD-011 on 2022/04/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExcerciseTimerApp/ExcerciseTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExcerciseTimerAppApp.swift
+//  ExcerciseTimerApp
+//
+//  Created by IACD-011 on 2022/04/13.
+//
+
+import SwiftUI
+
+@main
+struct ExcerciseTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
